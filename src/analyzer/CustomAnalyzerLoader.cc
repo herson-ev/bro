feeff7b5723d0724ff3abc4620bd9c3ab1592880
Herson Esquivel-Vargas <herson.ev@gmail.com>
2015-08-28 23:02:33 +0200

    Added a thread that polls the configuration file and updates the map of allowed analyzers.

--- src/analyzer/CustomAnalyzerLoader.cc
+++ src/analyzer/CustomAnalyzerLoader.cc
@@ -4,24 +4,10 @@ CustomAnalyzerLoader* CustomAnalyzerLoader::single = NULL;
 
 CustomAnalyzerLoader::CustomAnalyzerLoader()
 {
-    string line;
-    ifstream configFile(FILE_PATH);
-    if (configFile.is_open())
-    {
-        while (getline(configFile, line))
-        {
-            string proto, value;
-            stringstream input_stringstream(line);
-            getline(input_stringstream, proto, ' ');
-            getline(input_stringstream, value, ' ');
-            
-            dpi_protocols.insert(pair<string, bool>(proto, std::stoi(value)));                    
-        }
-        configFile.close();
-    } else
-    {
-        cerr << "Unable to open file " << FILE_PATH << endl;
-    }
+	read_config_file_now();
+	
+	thread polling_thread(&CustomAnalyzerLoader::poll_config_file, this);
+	polling_thread.detach();	
 }
 
 CustomAnalyzerLoader::~CustomAnalyzerLoader()
@@ -40,5 +26,62 @@ CustomAnalyzerLoader* CustomAnalyzerLoader::getInstance()
 
 bool CustomAnalyzerLoader::apply_dpi(string protocol)
 {
-    return CustomAnalyzerLoader::dpi_protocols.find(protocol)->second;
+    return dpi_protocols.find(protocol)->second;
+}
+
+/**
+ * This function reads the configuration file and loads it to a memory map
+ * called: map<string, bool> dpi_protocols.
+ * It is called from the Constructor when the class is initialized
+ */ 
+void CustomAnalyzerLoader::read_config_file_now()
+{
+	map<string, bool> tmp_map;
+	string line;
+    ifstream configFile(CustomAnalyzerLoader::FILE_PATH);
+    if (configFile.is_open())
+    {
+        while (getline(configFile, line))
+        {
+            string proto, value;
+            stringstream input_stringstream(line);
+            getline(input_stringstream, proto, ' ');
+            getline(input_stringstream, value, ' ');
+            
+            tmp_map.insert(pair<string, bool>(proto, std::stoi(value)));                    
+        }
+        configFile.close();
+    } else
+    {
+        cerr << "Unable to open file " << FILE_PATH << endl;
+    }
+    swap(tmp_map, dpi_protocols);
+    //print_map_ds();
+}
+
+/**
+ * One thread will keep polling the config file just in case some change in
+ * the policy is applied.
+ */ 
+void CustomAnalyzerLoader::poll_config_file()
+{
+	while(true)
+	{
+		this_thread::sleep_for(chrono::milliseconds(MILLIS));
+		read_config_file_now();
+	}
+}
+
+/**
+ * Prints the map data structure for debugging purposes
+ * 
+ */
+void CustomAnalyzerLoader::print_map_ds()
+{
+	for (map<string, bool>::iterator it = CustomAnalyzerLoader::dpi_protocols.begin(); 
+			it != CustomAnalyzerLoader::dpi_protocols.end(); ++it)
+	{
+		cout << it->first << " : " << it->second << endl;
+	}
+	cout << endl;
 }
--- src/analyzer/CustomAnalyzerLoader.h
+++ src/analyzer/CustomAnalyzerLoader.h
@@ -8,6 +8,10 @@
 #include <string>
 #include <sstream>
 #include <fstream>
+// Two libraries used for polling the config file every "x" seconds
+#include <thread>
+#include <chrono>
+
 using namespace std;
 
 class CustomAnalyzerLoader {
@@ -18,9 +22,15 @@ public:
 
 private:
     CustomAnalyzerLoader();
+    void read_config_file_now();
+    void print_map_ds();
     static CustomAnalyzerLoader* single;
-    const char* FILE_PATH = "/home/bro/config.txt";
+    //const char* FILE_PATH = "/home/bro/config.txt";
+    const char* FILE_PATH = "/home/herson/config.txt";
     map<string, bool> dpi_protocols;
+    const unsigned int MILLIS = 3000;
+
+    void poll_config_file();
 };
 
 #endif	/* CUSTOMANALYZERLOADER_H */
